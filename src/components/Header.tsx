18b26e11522c414b42414543d592153083dafd97
efzr <emerson.zapatarivas@gmail.com>
2024-05-24 20:29:31 -0600

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,46 @@
-import { Link } from "react-router-dom"
-import { BiMoon, BiSun, BiUser, BiSearch } from "react-icons/bi"
-import useDarkTheme from "../hooks/useDarkTheme"
-import "../styles/Header.css"
-import useTask from "../hooks/useTask"
+import { Link } from "react-router-dom";
+import { BiMoon, BiSun, BiUser, BiSearch } from "react-icons/bi";
+import useDarkTheme from "../hooks/useDarkTheme";
+import "../styles/Header.css";
+import useTask from "../hooks/useTask";
 
 export default function Header() {
-  const { toggleFilter, switchToggleFilter, theme, switchTheme } = useTask()
-  useDarkTheme(theme)
+  const { toggleFilter, switchToggleFilter, theme, switchTheme } = useTask();
+  useDarkTheme(theme);
 
   return (
     <header className="header">
       <nav className="nav container">
         <Link to="/" className="nav__logo">
-          Task<br />
+          Task
+          <br />
           <span>Master</span>
         </Link>
 
         <div className="nav__actions">
           <div className="nav__filter">
-            <BiSearch className="nav__icon-search" onClick={switchToggleFilter} />
-            <input type="text" placeholder="Search..." className={`nav__input-search ${toggleFilter && "nav__input-search-active"}`} />
+            <BiSearch
+              className="nav__icon-search"
+              onClick={switchToggleFilter}
+            />
+            <input
+              type="text"
+              placeholder="Search..."
+              id="filter"
+              name="filter"
+              className={`nav__input-search ${
+                toggleFilter && "nav__input-search-active"
+              }`}
+            />
           </div>
           <BiUser className="nav__icon" />
           {theme ? (
-            <BiSun
-              className="nav__icon"
-              onClick={switchTheme}
-            />
+            <BiSun className="nav__icon" onClick={switchTheme} />
           ) : (
-            <BiMoon
-              className="nav__icon"
-              onClick={switchTheme}
-            />
+            <BiMoon className="nav__icon" onClick={switchTheme} />
           )}
         </div>
       </nav>
     </header>
-  )
+  );
 }
