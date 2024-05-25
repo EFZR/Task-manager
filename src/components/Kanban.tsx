cdaddd20fec69c1155f7cbd8dc87fa1c1a7799a5
efzr <emerson.zapatarivas@gmail.com>
2024-05-25 00:50:09 -0600

    simple kanban completed succesfully2

--- src/components/Kanban.tsx
+++ src/components/Kanban.tsx
@@ -1,5 +1,76 @@
+import useTask from "../hooks/useTask";
+import { BiPlus } from "react-icons/bi";
+import "../styles/Kanban.css";
+
 export default function Kanban() {
+  const { currentProject } = useTask();
+  const { tasks } = currentProject;
+
   return (
-    <div>Kanban</div>
-  )
+    <div className="kanban__container grid">
+      <div className="kanban__slider">
+        <div className="kanban__list grid">
+          <div className="kanban__list-header">
+            <h3 className="kanban__list-title">To Do</h3>
+          </div>
+          {tasks.map((task) => (
+            <article className="kanban__card grid" key={task.id}>
+              <div className="color__flag"></div>
+              <h3 className="kanban__card-title">{task.title}</h3>
+              <p>{task.description}</p>
+            </article>
+          ))}
+          <div className="kanban__list-footer">
+            <button type="button" className="kanban__card-button">
+              <BiPlus className="kanban__card-icon" />
+              Add a Card
+            </button>
+          </div>
+        </div>
+        <div className="kanban__list grid">
+          <div className="kanban__list-header">
+            <h3 className="kanban__list-title">In Process</h3>
+          </div>
+          {tasks.map((task) => (
+            <article className="kanban__card grid" key={task.id}>
+              <div className="color__flag"></div>
+              <h3 className="kanban__card-title">{task.title}</h3>
+              <p>{task.description}</p>
+            </article>
+          ))}
+          {tasks.map((task) => (
+            <article className="kanban__card grid" key={task.id}>
+              <div className="color__flag"></div>
+              <h3 className="kanban__card-title">{task.title}</h3>
+              <p>{task.description}</p>
+            </article>
+          ))}
+          <div className="kanban__list-footer">
+            <button type="button" className="kanban__card-button">
+              <BiPlus className="kanban__card-icon" />
+              Add a Card
+            </button>
+          </div>
+        </div>
+        <div className="kanban__list grid">
+          <div className="kanban__list-header">
+            <h3 className="kanban__list-title">Done</h3>
+          </div>
+          {tasks.map((task) => (
+            <article className="kanban__card grid" key={task.id}>
+              <div className="color__flag"></div>
+              <h3 className="kanban__card-title">{task.title}</h3>
+              <p>{task.description}</p>
+            </article>
+          ))}
+          <div className="kanban__list-footer">
+            <button type="button" className="kanban__card-button">
+              <BiPlus className="kanban__card-icon" />
+              Add a Card
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Workspace.tsx
+++ src/pages/Workspace.tsx
@@ -1,9 +1,14 @@
+import Kanban from "../components/Kanban";
+import useTask from "../hooks/useTask";
+
 export default function Workspace() {
+  const { currentProject } = useTask();
   return (
     <div>
-      <section className="section">
-        <h1 className="section__title">Workspace</h1>
+      <section className="section container">
+        <h1 className="section__title">{currentProject.name}</h1>
+        <Kanban />
       </section>
     </div>
-  )
+  );
 }
